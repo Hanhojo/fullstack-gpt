a9c1675aaf83a4121ff9a1a0fa4a2a1f59a84fae
Hanhojo <145976977+Hanhojo@users.noreply.github.com>
2024-01-28 13:51:49 +0900

    first commit

---
+++ pages/01_DocumentGPT.py
@@ -0,0 +1,94 @@
+from langchain.prompts import ChatPromptTemplate
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
+from langchain.storage import LocalFileStore
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
+import streamlit as st
+st.set_page_config(
+    page_title="DocumentGPT",
+    page_icon="📃",
+)
+
+@st.cache_data(show_spinner="Embedding files...")
+
+def embed_file(file):
+ 
+    file_content = file.read()
+    file_path = f"./.cache/files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+    cache_dir = LocalFileStore(file.name)
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    loader = UnstructuredFileLoader("./files/chapter_two.txt")
+    docs = loader.load_and_split(text_splitter=splitter)
+    embeddings = OpenAIEmbeddings()
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
+    vectorstore = FAISS.from_documents(docs, cached_embeddings)
+    retriever = vectorstore.as_retriever()
+    return retriever
+
+
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.markdown(message)
+    if save:
+        st.session_state["message"].append({"message":message, "role":role})
+
+
+def paint_history():
+    for message in st.session_state["message"]:
+        send_message(
+            message["message"],
+            message["role"],
+            save=False,
+        )
+
+template = ChatPromptTemplate.from_messages(
+    [
+        (
+            "syste",
+            """
+            Answer the quetion using ONLY the following context. If you don't know the answer just say you don't know. DON'T make anything up.
+            context:{context}
+            """,
+        ),
+        ("human","{question}"),
+    ]
+)
+st.title("DocumentGPT")
+
+st.markdown(
+    """
+Welcome!
+            
+Use this chatbot to ask questions to an AI about your files!
+
+Upload your files on the sidebar.
+"""
+)
+with st.sidebar:
+  file = st.file_uploader(
+    "Upload a .txt .pdf or .docx file",
+    type=["pdf", "txt", "docx"],
+)
+
+if file:
+    retriever = embed_file(file)
+    send_message("I'm ready! Ask away!","ai", save=False)
+    paint_history()
+    message = st.chat_input("Ask anything about your file...")
+    if message:
+        send_message(message, "human")
+        docs = retriever.invoke(message)
+        docs = "\n\n".join(document.page_content for document in docs)
+        prompt = template.format_messages(context=docs, question=message)
+        st.write(prompt)
+
+
+else:
+    st.session_state["message"] = []
